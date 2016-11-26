8ab2eaf62980adc5fd28625a026888311aac5a1f
pbywater <philippabywater@gmail.comphilippabywater$ git config --global user.email philippabywater@gmail.comgit config --global user.email philippabywater@gmail.comgit config --global user.name pbywatergit config --global user.email test@youremail.com git config --global user.name Bhumi>
2016-11-26 22:16:05 +0000

    Adding sounds and images

---
+++ javascript.js
@@ -0,0 +1,44 @@
+$(function() {
+	$('body').hide();
+    $('body').fadeIn(1000);
+});
+
+       var audio = document.getElementById("audio");
+       var audio1 = document.getElementById("audio1");
+       var audio2 = document.getElementById("audio2");
+       var audio3 = document.getElementById("audio3");
+       var audio4 = document.getElementById("audio4");
+       var audio5 = document.getElementById("audio5");
+
+  function play(){
+
+       audio.play();
+
+       }
+
+         function pause(){
+       audio.pause();
+
+       }
+
+ function play1(){
+
+       audio1.play();
+
+       }
+
+         function pause1(){
+       audio1.pause();
+
+       }
+
+        function play2(){
+
+       audio2.play();
+
+       }
+
+         function pause2(){
+       audio2.pause();
+
+       }
