ca5353395be1343be2a0a01ec273af80afe44a30
pbywater <philippabywater@gmail.comphilippabywater$ git config --global user.email philippabywater@gmail.comgit config --global user.email philippabywater@gmail.comgit config --global user.name pbywatergit config --global user.email test@youremail.com git config --global user.name Bhumi>
2016-11-27 21:01:24 +0000

    add favicon and button

--- javascript.js
+++ javascript.js
@@ -1,14 +1,55 @@
 $(function() {
 	$('body').hide();
     $('body').fadeIn(1000);
+
 });
 
-       var audio = document.getElementById("audio");
-       var audio1 = document.getElementById("audio1");
-       var audio2 = document.getElementById("audio2");
-       var audio3 = document.getElementById("audio3");
-       var audio4 = document.getElementById("audio4");
-       var audio5 = document.getElementById("audio5");
+// $(window).scroll(function() {
+// 	var image = $('#image3');
+// 	 var scrollTop = image.offset();
+// 	 scrollTop = scrollTop.top;
+//      var windowHeight = $(window).height(); 
+//      var scrollToTop = windowHeight - scrollTop; 
+//     console.log(scrollTop);
+    
+
+// })
+
+// BACK TO TOP BUTTON
+
+// browser window scroll (in pixels) after which the "back to top" link is shown
+var offset = 100,
+  //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+  offset_opacity = 2000, //1200px??
+  //duration of the top scrolling animation (in ms)
+  scroll_top_duration = 700,
+  //grab the "back to top" link
+  $back_to_top = $('.cd-top');
+
+//hide or show the "back to top" link
+$(window).scroll(function(){
+  ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+  if( $(this).scrollTop() > offset_opacity ) { 
+    $back_to_top.addClass('cd-fade-out');
+  }
+});
+
+//smooth scroll to top
+$back_to_top.on('click', function(event){
+  event.preventDefault();
+  $('body,html').animate({
+    scrollTop: 0 ,
+  }, scroll_top_duration
+  );
+});
+// END OF BACK TO TOP BUTTON
+
+       document.getElementById("audio").loop = true;
+       document.getElementById("audio1").loop = true;
+       document.getElementById("audio2").loop = true;
+       document.getElementById("audio3").loop = true;
+       document.getElementById("audio4").loop = true;
+       document.getElementById("audio5").loop = true;
   
 
   //sound
@@ -69,3 +110,6 @@ $(function() {
   function pause5(){
        audio5.pause();
        }
+
+$('#image').onClick()
+
