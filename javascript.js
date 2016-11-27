97f0be3191817ff35650f27d98c2fb1b007773cd
pbywater <philippabywater@gmail.comphilippabywater$ git config --global user.email philippabywater@gmail.comgit config --global user.email philippabywater@gmail.comgit config --global user.name pbywatergit config --global user.email test@youremail.com git config --global user.name Bhumi>
2016-11-27 00:11:20 +0000

    Responsive changes

--- javascript.js
+++ javascript.js
@@ -9,40 +9,63 @@ $(function() {
        var audio3 = document.getElementById("audio3");
        var audio4 = document.getElementById("audio4");
        var audio5 = document.getElementById("audio5");
+  
+
+  //sound
 
   function play(){
-
        audio.play();
-
        }
-
          function pause(){
        audio.pause();
-
        }
+
+//sound1
 
  function play1(){
-
        audio1.play();
-
        }
-
          function pause1(){
        audio1.pause();
-
        }
 
-        function play2(){
+//sound2
 
+  function play2(){
        audio2.play();
-
        }
 
-         function pause2(){
+  function pause2(){
        audio2.pause();
+       }
 
+//sound3
+
+  function play3(){
+       audio3.play();
+       }
+
+  function pause3(){
+       audio3.pause();
+       }
+
+ //sound4
+
+  function play4(){
+       audio4.play();
+       }
+
+  function pause4(){
+       audio4.pause();
        }
 
 
+//sound5
    
-  
+  function play5(){
+       audio5.play();
+       }
+
+  function pause5(){
+       audio5.pause();
+       }
